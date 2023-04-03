14ce8f6b796c54c874fd2d99a3216c0d436b4ce5
anton <anton_oskin@yahoo.com>
2023-04-03 20:07:27 +0530

    #3 Добавил стабы методов получения максимальной и минимальной стоимости

--- TradeMaster/Binance/BinanceConnector.cs
+++ TradeMaster/Binance/BinanceConnector.cs
@@ -13,8 +13,26 @@ public class BinanceConnector
     /// <summary>
     /// Метод для продажи криптовалюты на Binance
     /// </summary>
-    public void CellCoins()
+    public bool CellCoins(Coins coin)
     {
-        
+        return true;
+    }
+
+    /// <summary>
+    /// Получить максимальную стоимость в определенном интервале
+    /// </summary>
+    /// <returns></returns>
+    public decimal GetMaxPrice(Coins coin, DateTime startDateTime, DateTime endDateTime)
+    {
+        return 0;
+    }
+    
+    /// <summary>
+    /// Получить минимальную стоимость в определенном интервале
+    /// </summary>
+    /// <returns></returns>
+    public decimal GetMinPrice(Coins coin, DateTime startDateTime, DateTime endDateTime)
+    {
+        return 0;
     }
 }
