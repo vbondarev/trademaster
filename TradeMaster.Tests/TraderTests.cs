97332d00e4c36b5b106281401dbf776313beee37
anton <anton_oskin@yahoo.com>
2023-04-05 21:42:30 +0530

    Тест для проверки формирования модели истории

--- TradeMaster.Tests/TraderTests.cs
+++ TradeMaster.Tests/TraderTests.cs
@@ -20,6 +20,28 @@ public class TraderTests
             .Setup(m => m.GetLastCoinPrice(Coins.BTC))
             .Returns(new CoinPriceModel{Coin = Coins.BTC, Price = 28500, Time = DateTime.Now});
         
+        mockConnector
+            .SetupSequence(m => m.GetMaxPrice(Coins.BTC, It.IsAny<DateTime>(),It.IsAny<DateTime>()))
+            .Returns(28500)
+            .Returns(28450)
+            .Returns(28470)
+            .Returns(28200)
+            .Returns(28300)
+            .Returns(28500)
+            .Returns(28200)
+            .Returns(28100);
+        
+        mockConnector
+            .SetupSequence(m => m.GetMinPrice(Coins.BTC, It.IsAny<DateTime>(),It.IsAny<DateTime>()))
+            .Returns(28450)
+            .Returns(28470)
+            .Returns(28200)
+            .Returns(28300)
+            .Returns(28250)
+            .Returns(28200)
+            .Returns(28100)
+            .Returns(28050);
+        
         var riskHandler = new RiskManagementHandler();
         var tradeHandler = new TradeHandler(mockConnector.Object);
         var trader = new Trader(mockConnector.Object, tradeHandler, riskHandler);
